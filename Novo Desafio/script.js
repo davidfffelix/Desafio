a27650ec6cb3672d03d53618afa73f1f96507d4e
David Felix <davidffelix@hotmail.com>
2022-02-16 09:28:10 -0300

    Update

---
+++ Novo Desafio/script.js
@@ -0,0 +1,11 @@
+const header = {
+    "Content-Type": "application/json",
+}
+  
+fetch('http://localhost:3000/indicadores', {header})
+.then(res => res.json())
+.then(retorno => {
+    document.querySelector('#ipca').value = retorno[1].valor + "%"
+    document.querySelector('#cdi').value = retorno[0].valor + "%"
+})
+
